f18e6d0d90d67782aac693992c889c459bc4b9a5
Themis Papameletiou <themicp@gmail.com>
2018-01-06 20:29:19 +0200

    Basic server setup and employee controller

--- api/index.js
+++ api/index.js
@@ -1,19 +1,33 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 const config = require('./config.json');
+const EmployeeController = require('./controllers/EmployeeController');
 
 const dbConfig = config.db;
 const connection = mysql.createConnection({
     host: dbConfig.host,
     user: dbConfig.user,
     password: dbConfig.pass,
+    database : dbConfig.name
+}).then((conn) => {
+    setupServer(conn);
 });
 
-const app = express();
+function setupServer(connection) {
+    const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
+    app.get('/', (req, res) => {
+        res.send('Hello world!');
+    });
 
-app.listen(config.port);
-console.log('Express server listening on port ' + config.port + '..');
+    app.get('/employees/list', (req, res) => {
+        const employees = EmployeeController.list(connection).then(rows => {
+            console.log(rows);
+        });
+
+        res.send('it works');
+    });
+
+    app.listen(config.port);
+    console.log('Express server listening on port ' + config.port + '..');
+}
