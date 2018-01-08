956ba2ba591e5e275f9de8a6e6c0e4556966331c
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2018-01-09 00:00:10 +0200

    Generate queries

--- api/db.js
+++ api/db.js
@@ -1,11 +1,42 @@
+const _ = require('lodash');
 const mysql = require('mysql2/promise');
 const config = require('./config.json');
 const dbConfig = config.db;
+
+function objectToQueryFields(fields) {
+    return Object.keys(fields).map((key) => key + '= :' + key);
+}
+
 const connection = mysql.createConnection({
     host: dbConfig.host,
     user: dbConfig.user,
     password: dbConfig.pass,
     database : dbConfig.name
+}).then(connection => { 
+    connection.insert = async function (table, fields) {
+        let assignments = objectToQueryFields(fields);
+        query = `INSERT INTO ${table} SET ` + assignments.join(',');
+        console.log(query);
+        console.log(fields);
+        let result = await connection.execute(query, fields);
+        return result;
+    }
+
+    connection.update = async function (table, fields, conditions) {
+        let assignments = objectToQueryFields(fields);
+        console.log(assignments);
+        let query = `UPDATE ${table} SET ` + assignments.join(',');
+        if (conditions && Object.keys(conditions).length != 0){
+            query += ` WHERE ` + assignments.join(" AND ");
+        }
+        console.log(query);
+       // let result = await connection.execute(query, 
+       //     Object.assign({}, fields, conditions)
+        //);
+
+        return result
+    }
+    return connection;
 })
 
-module.exports = connection;
+module.exports = connection; 
--- api/index.js
+++ api/index.js
@@ -4,11 +4,16 @@ const Vehicle = require('./controllers/Vehicle');
 const Client = require('./controllers/Client');
 const Store = require('./controllers/Store');
 const Employee = require('./controllers/Employee');
+const bodyParser = require('body-parser');
 
 (async () => {
     const db = await require('./db')
 
     const app = express();
+
+	app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+	app.use(bodyParser.json()); // for parsing application/json
+
 
     app.get('/', (req, res) => {
         res.send('Hello world!');
@@ -18,7 +23,7 @@ const Employee = require('./controllers/Employee');
     new Client(app.route('/clients/:clientId?'))
     new Store(app.route('/stores/:storeId?'))
     new Employee(app.route('/employee/:employeeId?'))
-
+	
     app.route('/stores/:storeId/vehicles/')
         .get((req, res) => {
             res.send('Show vehicles of store!');
