936b9a18c66b92641bde4441825e2f141d74f65f
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2018-01-09 20:44:51 +0200

    GET Vehicles

--- api/controllers/Vehicle.js
+++ api/controllers/Vehicle.js
@@ -1,8 +1,19 @@
 const Routable = require('./Routable');
+const order_field = require('../lib').order_field;
 
 class Vehicle extends Routable {
-    get(req, res) {
-        res.send('Show vehicle profile!');
+    async get(req, res) {
+        let result;
+        let conditions = {};
+        if (!req.params.vehicleId) {
+            let orderBy = [order_field("brand", "ASC"), order_field("model", "ASC")];
+            [result] = await this.db.select('vehicles', null, null, orderBy);
+        }
+        else {
+            [result] = await this.db.select('vehicles', [], {vehicle_id: req.params.vehicleId}, []);
+        }
+        console.log(result);
+        res.send(result);
     }
 
     post() {
