620d50cddec745c24222f3c5e384292b136809df
Themis Papameletiou <themicp@gmail.com>
2018-01-09 22:40:30 +0200

    Add Store page with basic vehicle list

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route } from 'react-router'
 import Register from './Register';
 import Home from './Home';
+import Store from './Store';
 
 class App extends Component {
     render() {
@@ -10,6 +11,7 @@ class App extends Component {
             <div className='App'>
                 <Route path='/register' component={Register} />
                 <Route path='/home' component={Home} />
+                <Route path='/store/:storeId' component={Store} />
             </div>
         );
     }
