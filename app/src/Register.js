92ed501a337dbe9655b7dc6059eef019c7372d04
Themis Papameletiou <themicp@gmail.com>
2018-01-09 16:55:32 +0200

    Store register form data in state, dynamically generate the elements, track changes

--- app/src/Register.js
+++ app/src/Register.js
@@ -4,69 +4,94 @@ import FlatButton from 'material-ui/FlatButton';
 import TextField from 'material-ui/TextField';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
+import $ from 'jquery';
 
 class Register extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            first_name: '',
+            last_name: '',
+            street_name: '',
+            street_number: '',
+            postal_code: '',
+            country: '',
+            identity_type: 1,
+            identity_number: '',
+        };
+    }
+
+    handleRegister() {
+        console.log('handler');
+    }
+
+    handleInputChange = (event, value) => {
+        event.persist();
+        this.setState(state => state[event.target.name] = value);
+    }
+
+    handleDrowdownChange = (name, event, key, value) => {
+        const state = this.state;
+        state[name] = value;
+        this.setState(state);
+    }
+
     render() {
+        const fields = [
+            {name: 'first_name', label: 'First Name'},
+            {name: 'last_name', label: 'Last Name'},
+            {name: 'street_name', label: 'Street Name'},
+            {name: 'street_number', label: 'Street Number'},
+            {name: 'postal_code', label: 'Postal Code'},
+            {name: 'country', label: 'Country'},
+            {name: 'identity_type', label: 'Identity Type', options: ['Identity Card', 'Passport']},
+            {name: 'identity_number', label: 'Identification Number'},
+        ];
+
+        let inputElements = fields.map((item, index) => {
+            if (item.options) {
+                const {options} = item;
+
+                const optionsElements = options.map((item, index) => <MenuItem key={index} value={index} primaryText={item} />);
+
+                return (
+                    <DropDownMenu
+                        autoWidth={false}
+                        key={index}
+                        style={{width: '100%'}}
+                        onChange={this.handleDrowdownChange.bind(this, item.name)}
+                        name={item.name}
+                        value={this.state[item.name]}>
+                        {optionsElements}
+                    </DropDownMenu>
+                );
+            } else {
+                return (<TextField
+                    key={index}
+                    name={item.name}
+                    onChange={this.handleInputChange}
+                    value={this.state[item.name]}
+                    fullWidth
+                    floatingLabelText={item.label}
+                    floatingLabelFixed={true}
+                />);
+            }
+        });
+        
         return (
             <div className='register'>
 				<Card>
 					<CardHeader title='Create a new account' />
 
                     <CardMedia className='cardMedia'>
-                        <TextField
-                            fullWidth
-                            floatingLabelText='First Name'
-                            floatingLabelFixed={true}
-                        />
-
-                        <TextField
-                            fullWidth
-                            floatingLabelText='Last Name'
-                            floatingLabelFixed={true}
-                        />
-
-                        <TextField
-                            fullWidth
-                            floatingLabelText='Street Name'
-                            floatingLabelFixed={true}
-                        />
-
-                        <TextField
-                            fullWidth
-                            floatingLabelText='Street Number'
-                            floatingLabelFixed={true}
-                        />
-
-                        <TextField
-                            fullWidth
-                            floatingLabelText='Postal Code'
-                            floatingLabelFixed={true}
-                        />
-
-                        <TextField
-                            fullWidth
-                            floatingLabelText='Country'
-                            floatingLabelFixed={true}
-                        />
-
-						<DropDownMenu
-							autoWidth={false}
-                            style={{width: '100%'}}
-                            value={0}
-						>
-							<MenuItem value={0} primaryText='Identity Card' />
-							<MenuItem value={1} primaryText='Passport' />
-						</DropDownMenu>
-
-                        <TextField
-                            fullWidth
-                            floatingLabelText='Identification Number'
-                            floatingLabelFixed={true}
-                        />
+                        <div>
+                            {inputElements}
+                        </div>
                     </CardMedia>
 
 					<CardActions>
-						<FlatButton label='Register' />
+						<FlatButton label='Register' onClick={this.handleRegister} />
 					</CardActions>
 				</Card>
             </div>
