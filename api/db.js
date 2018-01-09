9a61a6bdcd19f3aed21a21a75ea52b3cff38e187
Themis Papameletiou <themicp@gmail.com>
2018-01-09 05:12:55 +0200

    Fix placeholders use in queries

--- api/db.js
+++ api/db.js
@@ -4,7 +4,11 @@ const config = require('./config.json');
 const dbConfig = config.db;
 
 function objectToQueryFields(fields) {
-    return Object.keys(fields).map((key) => key + '= :' + key);
+    let keys = Object.keys(fields);
+    let placeholders = keys.map((key) => key + ' = ?');
+    let fieldValues = keys.map((key) => fields[key]);
+
+    return [placeholders, fieldValues]
 }
 
 const connection = mysql.createConnection({
@@ -14,25 +18,24 @@ const connection = mysql.createConnection({
     database : dbConfig.name
 }).then(connection => { 
     connection.insert = async function (table, fields) {
-        let assignments = objectToQueryFields(fields);
-        query = `INSERT INTO ${table} SET ` + assignments.join(',');
-        console.log(query);
-        console.log(fields);
-        let result = await connection.execute(query, fields);
+        let [placeholders, fieldValues] = objectToQueryFields(fields);
+        let query = `INSERT INTO ${table} SET ` + placeholders.join(',');
+        let result = await connection.execute(query, fieldValues);
+
         return result;
     }
 
     connection.update = async function (table, fields, conditions) {
-        let assignments = objectToQueryFields(fields);
-        console.log(assignments);
-        let query = `UPDATE ${table} SET ` + assignments.join(',');
+        let [placeholders, fieldValues] = objectToQueryFields(fields);
+        let query = `UPDATE ${table} SET ` + placeholders.join(',');
         if (conditions && Object.keys(conditions).length != 0){
-            query += ` WHERE ` + assignments.join(" AND ");
+            let [conditionPlaceholders, conditionValues] = objectToQueryFields(conditions);
+            query += ` WHERE ` + conditionPlaceholders.join(" AND ");
         }
+
         console.log(query);
-       // let result = await connection.execute(query, 
-       //     Object.assign({}, fields, conditions)
-        //);
+
+        //TODO: merge fieldValues and conditionValues here
 
         return result
     }
