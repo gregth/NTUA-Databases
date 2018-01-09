dc9a1a09e87ffdacfde4ef08c595f22029a87f3d
Themis Papameletiou <themicp@gmail.com>
2018-01-09 20:46:58 +0200

    Show list of available stores

--- app/src/Home.js
+++ app/src/Home.js
@@ -2,9 +2,25 @@ import React, { Component } from 'react';
 import {Card, CardMedia} from 'material-ui/Card';
 import Divider from 'material-ui/Divider';
 import ReservationItem from './ReservationItem';
+import StoreItem from './StoreItem';
 import Subheader from 'material-ui/Subheader';
+import axios from 'axios';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            stores: null
+        };
+    }
+
+    componentWillMount() {
+        axios.get('http://localhost:3001/stores').then(response => {
+            this.setState({stores: response.data});
+        });
+    }
+
     render() {
         const data = {
             start_date: '11 Jan 2017',
@@ -14,6 +30,11 @@ class Home extends Component {
             reservation_id: 2,
             store_id: 1,
             amount: 143.2
+        }
+
+        let storeItems = [];
+        if (this.state.stores) {
+            storeItems = this.state.stores.map((item, index) => (<StoreItem key={index} store={item} />));
         }
 
         return (
@@ -29,12 +50,14 @@ class Home extends Component {
                             <div className='clear' />
                             <Subheader>Available stores</Subheader>
                             <Divider />
+                            {storeItems.length ? storeItems : ''}
+                            <div className='clear' />
                         </div>
                     </CardMedia>
 				</Card>
             </div>
         );
-    }
+   }
 }
 
 export default Home;
---
+++ app/src/StoreItem.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import {List, ListItem} from 'material-ui/List';
+
+class StoreItem extends Component {
+    render() {
+        const {store} = this.props;
+        const store_name = store.store_name;
+        const store_address = `${store.street_name} ${store.street_number}, ${store.postal_code} ${store.city}, ${store.country}`;
+
+        return (
+            <Card className='storeItem'>
+                <CardMedia className='cardMedia'>
+                    <img src='stores/store.png' alt='store' />
+                </CardMedia>
+				<CardTitle title={store_name} />
+				<CardText style={{paddingTop: 0}}>
+					<List>
+						<ListItem
+                            disabled={true}
+                            primaryText='Address'
+                            style={{padding: '8px 0'}}
+                            secondaryText={store_address}>
+                            <a className='list-right' target='_blank' href={'https://www.google.gr/maps/search/' + encodeURIComponent(store_address)}>Map</a>
+                        </ListItem>
+                    </List>
+				</CardText>
+				<CardActions style={{textAlign: 'center'}}>
+					<RaisedButton fullWidth={true} label='View vehicles' />
+				</CardActions>
+            </Card>
+        );
+    }
+}
+
+export default StoreItem;
