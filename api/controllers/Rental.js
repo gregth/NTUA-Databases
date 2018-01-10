4a564a21d9ed46090491dea6d8f96eaddc220f7c
Themis Papameletiou <themicp@gmail.com>
2018-01-11 01:53:25 +0200

    Add more returned fields from Rental, Reservation and Vehicle

--- api/controllers/Rental.js
+++ api/controllers/Rental.js
@@ -12,7 +12,9 @@ class Rental extends Routable {
                 table_alias: 'r',
                 allowed_search_keys: ['client_id'],
                 keys_lookup_table: {client_id: 'rv'},
-                fields:  [],
+                fields:  ['receiver_employee_id', 'deliverer_employee_id', 'r.reservation_id',
+                    'r.start_date', 'r.end_date', 'damage_score', 'rv.start_date as reservation_start_date',
+                    'rv.end_date as reservation_end_date', 'rv.vehicle_id', 'rv.client_id', 'rv.amount', 'rv.store_id'],
                 orderBy: [order_field('r.start_date', 'ASC')],
                 joins: [{ type: 'JOIN', table: 'reservations AS rv', on: 'rv.reservation_id = r.reservation_id'},
                     {type: 'JOIN', table: 'clients AS c', on: 'rv.client_id = c.client_id'}]
