d6f3b533ceca210c91fab1671673cdc8662ae5be
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2018-01-10 18:17:43 +0200

    GET/POST/PUT Employees

--- api/controllers/Employee.js
+++ api/controllers/Employee.js
@@ -2,21 +2,96 @@ const Routable = require('./Routable');
 
 class Employee extends Routable {
     async get(req, res) {
+        let result;
+        let conditions = {};
         if (!req.params.employeeId) {
-			let [rows, fields] = await this.db.execute('SELECT * FROM employees');
-            res.send(rows);
+            [result] = await this.db.select('employees');
         }
+        else {
+            [result] = await this.db.select('employees', [], {employee_id: req.params.employeeId}, []);
+        }
+
+        console.log(result);
+        if (result.length == 0) {
+            res.status(404);
+            res.send("Employee not found");
+            return;
+        }
+        res.status(200);
+        res.send(result);
+        return;
     }
 
-    post() {
+    async post(req, res) {
+		if (req.params.employeeId) {
+			res.status(500);
+			res.send("Operation not permited on individual employees");
+            return;
+		}
+
+		var needed_parameters = ["license_id",
+			"store_id", "identity_number", "first_name",
+			"last_name", "street_name", "postal_code", "city",
+			"country", "street_number", "role", "start_date", "end_date"];
+		let details = this.filter_keys(req.body, needed_parameters);
+		let [result] = await this.db.insert(`employees`, details)
+
+		console.log(result);
+		if (result.affectedRows != 1) {
+			res.status(500);
+			res.send("Error");
+            return;
+		}
+		res.send({employee_id: result.insertId});
+		res.status(200);
     }
 
-    put(req, res) {
-        res.send('Update employee profile!');
+    async put(req, res) {
+        if (!req.params.employeeId) {
+            res.status(500);
+			res.send("You must specify employee to update");
+            return;
+        }
+
+		var needed_parameters = ["license_id",
+			"store_id", "identity_number", "first_name",
+			"last_name", "street_name", "postal_code", "city",
+			"country", "street_number", "role", "start_date", "end_date"];
+        let details = this.filter_keys(req.body, needed_parameters);
+        console.log(details);
+        let conditions = {employee_id: req.params.employeeId};
+        let [result] = await this.db.update('employees', details, conditions)
+
+		console.log(result);
+        if (result.affectedRows != 1) {
+            res.status(500);
+            res.send("Error");
+            return;
+        }
+        res.status(200);
+        res.send(result);
+        return;
     }
 
-    delete(req, res) {
-        res.send('Delete employee profile!');
+    async delete(req, res) {
+        if (!req.params.employeeId) {
+            res.status(500);
+            res.send("You must specify employee to delete");
+            return;
+        }
+
+        let conditions = {employee_id: req.params.employeeId};
+        console.log(conditions);
+        let [result] = await this.db.delete('employees', conditions)
+
+		console.log(result);
+        if (result.affectedRows != 1) {
+            res.status(500);
+            res.send("Error");
+            return;
+        }
+        res.send("Succesfully deleted employee");
+        res.status(200);
     }
 }
 
