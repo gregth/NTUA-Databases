75367d12be21af8c703edda59f23e027a41d1563
Themis Papameletiou <themicp@gmail.com>
2018-01-10 18:41:28 +0200

    Abstract common logic for REST endpoints in Routable

--- api/controllers/Client.js
+++ api/controllers/Client.js
@@ -2,73 +2,27 @@ const Routable = require('./Routable');
 const order_field = require('../lib').order_field;
 
 class Client extends Routable {
-    async get(req, res) {
-        let result;
-        let conditions = {};
-        if (!req.params.clientId) {
-            var allowed_search_keys = ["identity_number", "first_name", "last_name"];
-            conditions = this.filter_keys(req.query, allowed_search_keys);
-            console.log(conditions);
+    constructor(router) {
+        super(router);
 
-            let fields = ["first_name", "last_name", "license_id", "identity_number"];
-            let orderBy = [order_field("first_name", "ASC"), order_field("last_name", "ASC")];
-            [result] = await this.db.select('clients', fields, conditions, orderBy);
-        }
-        else {
-            [[result]] = await this.db.select('clients', [], {client_id: req.params.clientId});
-            if (!result) {
-                res.status(404);
-                res.send("NOT FOUND");
+        this.options = {
+            table: 'clients',
+            get: {
+                optionalField: 'clientId',
+                allowed_search_keys: ['client_id', 'identity_number', 'first_name', 'last_name'],
+                fields: ['first_name', 'last_name', 'license_id', 'identity_number'],
+                orderBy: [order_field('first_name', 'ASC'), order_field('last_name', 'ASC')],
+            },
+            post: {
+                fields: ['identity_number', 'first_name', 'last_name', 'street_name', 'street_number', 'postal_code', 'city', 'country', 'email', 'password'],
+            },
+            put: {
+                fields: ['license_id', 'identity_number', 'first_name', 'last_name', 'street_name', 'street_number', 'postal_code', 'city', 'country'],
+            },
+            delete: {
+                queryField: 'client_id',
             }
         }
-        console.log(result);
-        res.send(result);
-    }
-
-    async post(req, res) {
-        let param = {
-            identity_number: req.body["identity_number"],
-            first_name: req.body["first_name"],
-            last_name: req.body["last_name"],
-            street_name: req.body["street_name"],
-            street_number: req.body["street_number"],
-            postal_code : req.body["postal_code"],
-            city: req.body["city"],
-            country: req.body["country"],
-            email: req.body["email"],
-            password: req.body["password"],
-        };
-        console.log(param);
-        let result = await this.db.insert('clients', param)
-        res.send('Create client profile!');
-        //console.log(result);
-
-    }
-
-    async put(req, res) {
-        let param = {
-            license_id: req.body["license_id"],
-            identity_number: req.body["identity_number"],
-            first_name: req.body["first_name"],
-            last_name: req.body["last_name"],
-            street_name: req.body["street_name"],
-            street_number: req.body["street_number"],
-            postal_code : req.body["postal_code"],
-            city: req.body["city"],
-            country: req.body["country"],
-            email: req.body["email"],
-            password: req.body["password"],
-        };
-        await this.db.update('clients', param);
-        res.send('Update client profile!');
-    }
-
-    delete(req, res) {
-        let param = {
-            client_id: req.body["client_id"],
-        };
-        //await this.db.delete('clients', param);
-        res.send('Delete client profile!');
     }
 }
 
--- api/controllers/Routable.js
+++ api/controllers/Routable.js
@@ -16,17 +16,81 @@ class Routable {
             return f.call(this, ...args);
         }
     }
-    get() {
-        throw 'Not implemented get()';
+
+    async get(req, res) {
+        let result;
+        let conditions = {};
+
+        let methodOptions = this.options.get;
+        let optionalField = methodOptions.optionalField;
+        if (!req.params[optionalField]) {
+            var allowed_search_keys = methodOptions.allowed_search_keys;
+            conditions = this.filter_keys(req.query, allowed_search_keys);
+
+            let fields = methodOptions.field;
+            let orderBy = methodOptions.orderBy;
+
+            [result] = await this.db.select(this.options.table, fields, conditions, orderBy);
+        }
+        else {
+            conditions[optionalField] = req.params[optionalField];
+            [[result]] = await this.db.select(this.options.table, [], conditions);
+
+            if (!result) {
+                res.status(404);
+                res.send("NOT FOUND");
+                return;
+            }
+        }
+
+        console.log(result);
+        res.send(result);
     }
-    post() {
-        throw 'Not implemented post()';
+
+    async post() {
+        let methodOptions = this.options.post;
+
+        let params = this.filter_keys(req.body, methodOptions.fields);
+        try {
+            let result = await this.db.insert(this.options.table, params)
+            res.status(200);
+            res.send('Success.');
+        } catch(e) {
+            console.log(e);
+            res.status(500);
+            res.send('Internal error.');
+        }
     }
-    put() {
-        throw 'Not implemented put()';
+
+    async put() {
+        let methodOptions = this.options.put;
+
+        let params = this.filter_keys(req.body, methodOptions.fields);
+        try {
+            let result = await this.db.update(this.options.table, params);
+            res.status(200);
+            res.send('Success.');
+        } catch(e) {
+            console.log(e);
+            res.status(500);
+            res.send('Internal error.');
+        }
     }
-    delete() {
-        throw 'Not implemented delete()';
+
+    async delete() {
+        let methodOptions = this.options.delete;
+
+        let param = {};
+        param[methodOptions.queryField] = req.body[methodOptions.queryField]
+        try {
+            let result = await this.db.delete('clients', param);
+            res.status(200);
+            res.send('Success.');
+        } catch(e) {
+            console.log(e);
+            res.status(500);
+            res.send('Internal error.');
+        }
     }
 
     filter_keys(params, allowed_keys) {
